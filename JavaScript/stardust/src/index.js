86b316c4ba87ca95fceb33b06004a329b1980fe0
Mandy Scott <mandy@cpan.org>
2023-10-23 15:27:00 +1100

    up to solution5

--- JavaScript/stardust/src/index.js
+++ JavaScript/stardust/src/index.js
@@ -7,8 +7,9 @@ import { includeBytes } from "fastly:experimental";
 // File path is relative to root of project, not to this file
 const welcomePage = includeBytes("./src/welcome-to-compute@edge.html");
 
-// Define our backend
-const backend_0 = "origin_0";
+// Specify the available backends in an Array for ease of use later
+// these should match the origins defined in your service (and toml for any locally defined backends)
+const backends = ["origin_0", "origin_1"];
 
 addEventListener("fetch", (event) => event.respondWith(handleRequest(event)));
 
@@ -29,13 +30,17 @@ async function handleRequest(event) {
   // Create a cache override.
   let cacheOverride = new CacheOverride("override", { ttl: 60 });
 
+  // Load balancing: pick a backend at random
+  let activeBackend = backends[Math.floor(Math.random() * backends.length)];
+  console.log("## Active backend is: " + activeBackend);
+ 
   let url = new URL(req.url);
   // If request is to the `/` path...
   //if ((url.pathname == "/") || (url.pathname == "/test.php")) {
     // simply pass through the reqest to the backend server
     // note: normally you should do some due diligence security and so on, rather than just a straight pass through
     let resp = await fetch( req, {
-      backend: backend_0,
+      backend: activeBackend,
       cacheOverride
     });
 
